69586c4cda6c6830a8185c4032a43f797a5eb580
Marcus Gerhold <marcus.gerhold@gmail.com>
2022-11-07 11:45:48 +0100

    Fixed typo

--- exercises/box.py
+++ exercises/box.py
@@ -1,5 +1,5 @@
 class Box:
-    """Class for the bx that constraints the bubble, most of the time."""
+    """Class for the box that constraints the bubble, most of the time."""
 
     def __init__(self):
         pass
